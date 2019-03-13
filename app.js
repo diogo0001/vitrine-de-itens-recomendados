d4c42087237e1af6a65be1719dc4eb6b07901b23
diogo0001 <diogo.t0001@gmail.com>
2019-03-12 23:56:02 -0300

    Deploy

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-const port = 8080;
+const port = process.env.PORT || 3000
 
 // carrega a pasta com todos os arquivos
 app.use(express.static('public'));
